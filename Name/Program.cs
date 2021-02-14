317dd4b930461a05a1ada083b42ff67ff7520ac1
Александр Могильный <blagimi@yandex.ru>
2021-02-14 19:08:59 +0500

    Добавьте файлы проекта.

---
+++ Name/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
