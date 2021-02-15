05d05cfc1c25805778d8d6b94b7a76f512ac2ca2
Александр Могильный <blagimi@yandex.ru>
2021-02-15 18:24:07 +0500

    2.5

---
+++ ConsoleInputDataConvert/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;                               // Подлючение пространства имён
+
+namespace ConsoleInputDataConvert           // Объявление новоого пространства имён
+{
+    class Program                           // Обьявление нового класаа
+    {
+        static void Main()                  // Объявление нового метода
+        {   // Действия метода
+            // Просим пользователя ввести имя
+            Console.WriteLine("Введите имя: ");
+            // Читаем ответ
+            string name = Console.ReadLine();
+            // Просим пользователя ввести возраст
+            Console.WriteLine("Введите возраст: ");
+            // Читаем ответ
+            int age = Convert.ToInt32(Console.ReadLine());
+            // Просим пользователя ввести рост
+            Console.WriteLine("Введтие рост: ");
+            // Читаем ответ
+            double height = Convert.ToDouble(Console.ReadLine());
+            // Просим пользователя ввести размер зарплаты
+            Console.WriteLine("Введите размер зарплаты: ");
+            // Читаем ответ
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+            // Выводим на экран значения введеных пользователем переменных
+            Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height} Зарплата: {salary}");
+        }                                   // Конец объявления метода Main
+    }                                       // Конец объявления класса Program
+}                                           // Конец объявления пространства имён 
